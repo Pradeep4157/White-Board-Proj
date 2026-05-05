cc136b7ba3a8daa7cefeeafbae19f59549c45241
Pradeep4157 <krp18539@gmail.com>
2026-05-05 17:18:33 +0530

    par done with useRef..

--- src/store/BoardProvider.js
+++ src/store/BoardProvider.js
@@ -1,21 +1,42 @@
 import boardContext from "./board-context.js";
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import { TOOL_ITEMS } from "../constants.js";
-const boardReducer = (state, action) => {};
-const initialBoardState = {};
+const boardReducer = (state, action) => {
+  switch (action.type) {
+    case "CHANGE_TOOL":
+      return {
+        ...state,
+        activeToolItem: action.payload.tool,
+      };
+    default:
+      return state;
+  }
+};
+const initialBoardState = {
+  activeToolItem: TOOL_ITEMS.LINE,
+  elements: [],
+};
+
 const BoardProvider = ({ children }) => {
   const [boardState, dispatchBoardAction] = useReducer(
     boardReducer,
     initialBoardState,
   );
+
   //   const [activeToolItem, setActiveToolItem] = useState(TOOL_ITEMS.LINE);
   //   const [elements, setElements] = useState([]);
 
   const handleToolItemClick = (tool) => {
-    setActiveToolItem(tool);
+    dispatchBoardAction({
+      type: "CHANGE_TOOL",
+      payload: {
+        tool,
+      },
+    });
   };
+
   const BoardContextValue = {
-    activeToolItem,
+    activeToolItem: boardState.activeToolItem,
     handleToolItemClick,
   };
   return (
