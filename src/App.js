2b786ea7cdfa4bbde00d7c4712d007c3ca0562c1
Pradeep4157 <krp18539@gmail.com>
2026-06-11 18:48:41 +0530

    some mor erororr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useRef, useEffect } from "react";
 import rough from "roughjs";
-import Board from "./components/Board";
+import Board from "./components/board";
 import ToolBar from "./components/ToolBar";
 import BoardProvider from "./store/BoardProvider.js";
 import ToolboxProvider from "./store/ToolboxProvider.js";
